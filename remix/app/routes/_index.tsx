9a739993317f1a4497a36afbc90c350736a5219f
Diego Perez <diego.perez@savia.net>
2025-04-02 09:53:52 +0200

    feat: Remix base tutorial

---
+++ remix/app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        This is a demo for Rosseta.
+        <br />
+        Check out{" "}
+        <a href="https://remix.run">the docs at remix.run</a>.
+      </p>
+    );
+  }
+  
