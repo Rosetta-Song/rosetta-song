f01f83d8ea0d4f09045bfa0d81776c4ab4faad64
Diego Perez <diegosalas@gmail.com>
2025-04-11 18:22:46 +0200

    fix: main page

--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -47,7 +47,7 @@ export default function App() {
   useEffect(() => {
     const searchField = document.getElementById("q");
     if (searchField instanceof HTMLInputElement) {
-      searchField.value = q || "";
+      searchField.value = q ?? "";
     }
   }, [q]);
 
@@ -60,18 +60,41 @@ export default function App() {
         <Links />
       </head>
       <body id="index-body">
-      <div className="flex flex-row">
-        <div className="basis-64">01</div>
-        <div className="basis-64">02</div>
-        <div className="basis-128">03</div>
       </div>
+        
+        <div className="static">
+            <div className="absolute top-20 left-220">   </div>
+            <div className="button-container absolute top-6 left-18">
+              <a href="./premium" className="text-blue-600 visited:text-purple-600">Premium</a>
+              <span className="mx-2">&nbsp;</span>
+              <a href="./support" className="text-blue-600 visited:text-purple-600">Support</a>
+             </div>
+            <span className="mx-2">&nbsp;</span>
+            
+              <div className="button-container absolute top-2 right-20">
+                  <button className="transform motion-safe:hover:scale-110 ">
+                  Sign up
+                </button>
+                <span className="mx-2">&nbsp;</span>
+                <button className="transform hover:scale-110 motion-reduce:transform-none">
+                  Log In
+                </button>
+              </div>
+            
+         </div>
+
+      
+        
       <div className="flex flex-row" id="index">
-        <div id="index-logo" className="basis-1/3">
+        <div id="index-logo" className="basis-1/4">
             
             <div id="logo-side-dark" aria-label="Rosseta Song"></div>
          
         </div> 
-          <div className="basis-2/3">
+        <div id="index-logo" className="basis-1/4">
+          <div id="logo-image-index" aria-label="Rosseta Song"></div>
+        </div>
+          <div className="basis-2/4">
             <Form
               id="index-search-form"
               role="search"
@@ -94,16 +117,23 @@ export default function App() {
               <div id="index-search-spinner" aria-hidden hidden={!searching} />
             </Form>
           </div>
+          <div className="basis-2/4">
                 <blockquote className="text-center text-2xl font-semibold text-gray-900 italic dark:text-white">
-                    Create your professional video using &nbsp;
+            
+               Create professional video with &nbsp;
                       <div className="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-[#1DB954]">
                          <span className="relative text-white dark:text-gray-950">Spotify</span>
                       </div>
-                    &nbsp; it&apos;s simple now with Rosseta AI.
+                    <br /> it&apos;s simple now!
                   </blockquote>
+             </div>    
            </div>
 
-
+           <div className="flex flex-row">
+            <div className="basis-64">01</div>
+            <div className="basis-64">02</div>
+            <div className="basis-128">03</div>
+           </div>
         <div
           className={
             navigation.state === "loading" && !searching ? "loading" : ""
@@ -112,11 +142,13 @@ export default function App() {
         >    </div>
             <div id="results">
                 <nav>
+             
                     {contacts?.length ? (
-                        <ul className="contact-list">
+                      
+                      <div className="columns-3 flex-col items-center p-7 rounded-2xl">
+              
                         {contacts.map((contact: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; album: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; avatar: string | undefined; artist: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; favorite: any; }) => (
-                            <li className="inline-flex py-4 first:pt-0 last:pb-0" key={contact.id}>
-                            <div className="ml-3 overflow-hidden">
+                         /*!-- <li className="inline-flex py-4 first:pt-0 last:pb-0" key={contact.id}></li> */
                               <NavLink
                                   className={({ isActive, isPending }) =>
                                   isActive ? "active" : isPending ? "pending" : ""
@@ -124,24 +156,34 @@ export default function App() {
                                   to={`contacts/${contact.id}?q=${q ?? "Top Hits"}`}
                               >
                                   {contact.name || contact.album ? (
-                                  <>
-                                                    <img className="h-20 w-20 rounded-full" src={contact.avatar} alt="" />
-                                                   
-                                                    <p className="text-sm font-medium text-gray-900 dark:text-white">{contact.name} {contact.album}</p>  
-                                                    <p className="truncate text-sm text-gray-500 dark:text-gray-400">{contact.artist}</p>
+                                  <>        
+                                  
+                                    <div className="flex items-center gap-4">
+                                        <img
+                                          className="flex-shrink-0 w-48 h-48 shadow-xl rounded-md"
+                                          alt=""
+                                          src={contact.avatar}
+                                        />
+                                        <div className="flex-grow">
+                                          <span className="block text-2xl font-medium">{contact.name}</span>
+                                          <span className="block font-medium text-sky-500">{contact.album}</span>
+                                          <span className="block gap-2 font-medium text-gray-600 dark:text-gray-400">
+                                            {contact.artist}
+                                          </span>
+                                        </div>
+                                      </div>
+                                
+                                            
                                   </>
                                   ) : (
                                   <i>No Name</i>
                                   )}{" "}
                                   {contact.favorite ? <span id="starts">★</span> : null }
                               </NavLink>
-                        
-                            </div>
-                            
-                            </li>
+
                             
                         ))}
-                        </ul>
+                        </div>
                     ) : (
                  
                         <p>No contacts available</p>
